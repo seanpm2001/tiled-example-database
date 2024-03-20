66783a0cb3be1106fe2788a81892f5c4b086cdf8
kari Barry <kezzsim@gmail.com>
2024-03-20 13:57:33 -0400

    Actually generate the data

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -6,12 +6,13 @@ import sys
 from tiled.catalog import from_uri
 from tiled.client import Context, from_context
 from tiled.server.app import build_app
+import pandas as pd
 
 uri = sys.argv[1]
 catalog = from_uri(uri, writable_storage="temp")
 with Context.from_app(build_app(catalog)) as context:
     client = from_context(context)
-    # Write data
+    # Write data generated test data
     for i in range(10000):
         client.write_array(
             [i],
@@ -22,3 +23,7 @@ with Context.from_app(build_app(catalog)) as context:
                 "bool": bool(i),
             },
         )
+    # Write in captured PandAblocks data
+    pandas_data = pd.read_json('./data/panda_output/export-docs-20240315_105425.jsonl', lines=True)
+    for index, row in pandas_data.iterrows():
+        client.write_array([0],metadata=row.to_dict())
